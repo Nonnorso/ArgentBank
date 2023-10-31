2429016cca82eee19a68edd682952282739a5149
Nonnorso <r.philippot@live.fr>
2023-10-31 08:48:27 +0100

    modification de l'editButton pour lui permettre d'afficher le formulaire de changement d'userName

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -1,8 +1,55 @@
-export default function EditButton () {
+import React, { useState } from 'react';
 
-    return(
-        <button className="edit-button">
+export default function EditButton() {
+    const [showForm, setShowForm] = useState(false);
+
+    const toggleForm = () => {
+        setShowForm(!showForm);
+    };
+
+    const handleSave = () => {
+        // Logique de sauvegarde des données ici
+        setShowForm(false);
+    };
+
+    const handleCancel = () => {
+        // Logique d'annulation ici
+        setShowForm(false);
+    };
+
+    return (
+        <div>
+            {showForm ? (
+                <div className='editButton_container'>
+                    <form className='editForm'>
+                        <h1 className='editFormTitle'>Edit user info</h1>
+                        <div className="editFormItems">
+                            <label htmlFor="userName">User Name : </label>
+                            <input type="text" id='userName' />
+                        </div>
+                        <div className="editionItems">
+                            <label htmlFor="firstName">First Name : </label>
+                            <input type="text" id='firstName' placeholder="firstName" disabled />
+                        </div>
+                        <div className="editItems">
+                            <label htmlFor="lastName">Last Name : </label>
+                            <input type="text" id='lastName' placeholder="lastName" disabled />
+                        </div>
+                        <div className="editSaveButton">
+                            <button type='submit' className='editFormButton' onClick={handleSave}>
+                                Save
+                            </button>
+                            <button className='editFormButton' onClick={handleCancel}>
+                                Cancel
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            ) : (
+                <button className="edit-button" onClick={toggleForm}>
                     Edit Name
-        </button>
-    )
+                </button>
+            )}
+        </div>
+    );
 }
