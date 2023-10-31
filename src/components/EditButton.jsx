4b8f18e1879a0f79d86e8af86ce05f221a5658ce
Nonnorso <r.philippot@live.fr>
2023-10-31 10:16:33 +0100

    attribution des données de userProfile dans les champs du formulaire de changement de nom

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function EditButton({ userProfile, token }) {
     const [showForm, setShowForm] = useState(false);
+    const [userData, setUserData] = useState({
+        userName: '',
+        firstName: '',
+        lastName: ''
+    });
+
+    useEffect(() => {
+        if (userProfile) {
+            setUserData({
+                userName: userProfile.userName,
+                firstName: userProfile.firstName,
+                lastName: userProfile.lastName
+            });
+        }
+    }, [userProfile]);
 
     const toggleForm = () => {
         setShowForm(!showForm);
     };
 
     const handleSave = () => {
-        // Logique de sauvegarde des données ici
         setShowForm(false);
     };
 
     const handleCancel = () => {
-        // Logique d'annulation ici
         setShowForm(false);
     };
 
@@ -25,15 +38,15 @@ export default function EditButton({ userProfile, token }) {
                         <h1 className='editFormTitle'>Edit user info</h1>
                         <div className="editFormItems">
                             <label htmlFor="userName">User Name : </label>
-                            <input type="text" id='userName' />
+                            <input type="text" id='userName' value={userData.userName} onChange={(e) => setUserData({ ...userData, userName: e.target.value })}  />
                         </div>
                         <div className="editionItems">
                             <label htmlFor="firstName">First Name : </label>
-                            <input type="text" id='firstName' placeholder="firstName" disabled />
+                            <input type="text" id='firstName' placeholder="firstName" value={userData.firstName} onChange={(e) => setUserData({ ...userData, firstName: e.target.value })} disabled />
                         </div>
                         <div className="editItems">
                             <label htmlFor="lastName">Last Name : </label>
-                            <input type="text" id='lastName' placeholder="lastName" disabled />
+                            <input type="text" id='lastName' placeholder="lastName" value={userData.lastName} onChange={(e) => setUserData({ ...userData, lastName: e.target.value })} disabled />
                         </div>
                         <div className="editSaveButton">
                             <button type='submit' className='editFormButton' onClick={handleSave}>
